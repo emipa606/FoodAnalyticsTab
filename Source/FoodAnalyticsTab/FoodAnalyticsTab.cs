b355885cb4a04d2853b1ec41f1a413b4a847812d
mugenZebra <c087892275@gmail.com>
2016-11-26 20:21:23 -0800

    made minor changes, constructor, more text analysis, and an unfinished prototype of generic prediction class and comments.

--- Source/FoodAnalyticsTab/FoodAnalyticsTab.cs
+++ Source/FoodAnalyticsTab/FoodAnalyticsTab.cs
@@ -117,15 +117,7 @@ namespace FoodAnalyticsTab
         private int daysUntilEndofWinter; // to February 5th
         private int daysUntilGrowingPeriodOver; // to 10th of Fall, Oct 5th
         private int daysUntilNextHarvestSeason; // to 10th of Spring, April 5th
-        static float hayNut = (from d in DefDatabase<ThingDef>.AllDefs.Where(x => x.defName == "Hay")
-                               select d).FirstOrDefault().ingestible.nutrition;
-        static float haygrass_yieldMax = GenMath.RoundRandom(
-            (from d in DefDatabase<ThingDef>.AllDefs.Where(x => x.defName == "PlantHaygrass")
-             select d).FirstOrDefault().plant.harvestYield );
-        static float haygrass_yieldMin = GenMath.RoundRandom(
-            (from d in DefDatabase<ThingDef>.AllDefs.Where(x => x.defName == "PlantHaygrass")
-             select d).FirstOrDefault().plant.harvestYield * 0.5f * 0.5f// 1st 0.5 is harvesting at 65% growth, 2nd 0.5 is lowest health.
-            );
+        static float hayNut = 0, haygrass_yieldMax = 0, haygrass_yieldMin = 0;
 
         private class LineGraph
         {
@@ -197,6 +189,7 @@ namespace FoodAnalyticsTab
                 return scrollPos_curr;
             }
         }
+
         List<LineGraph> graphList = new List<LineGraph>() {new LineGraph(nextNDays) };
 
         /* settings for each graph:
@@ -208,13 +201,209 @@ namespace FoodAnalyticsTab
            
            internal:
              SimpleCurveDrawerStyle
-             are settings changed
+             are settings changed        
+        */
+
+        /* prediction class:
+           making prediction 
+           types: crop/population/meat yield, vegie/meat/meal/drug/leather/population stock, 
            
+           type of update: 1st day, after
+           data structure
+           list of crops
+             yield
+             stock
+             consumption
+           
+           model:
+           1.
+           chicken -> egg--↘
+           animals -> meat -> kibble -> carnivores animal
+           merchant->       ^ meals -> conlonists
+           hunting ->       | training -> animals
+                            |           ^
+                            |__ work    |__ work
+           2.
+           growers -> haygrass -> hay -> herbivores
+                    ^           ^     ↘ kibble
+                    |           |
+                    |__planting |__harvesting
+           3.
+           grower -> rice plant -> rice -------------> meals -> colonist
+                  -> corn plant -> corn                      |
+                  -> potato plant -> potato                  |
+                  -> strawberry plant -> strawberry ----------
+           4.
+           cotton
+           muffalo     -> wool -> armchair, 
+           alpaca              -> clothing
+           Megatherium         -> medicine
+           Dromedary
+
+        */
+        /*
            data selector checkbox
            add graph button
         */
+        private class Prediction2
+        {
+            private class PlantGrowth
+            {
+                public PlantGrowth(float a, float b)
+                {
+                    Growth = a;
+                    GrowthPerTick = b;
+                }
+                public float Growth { get; set; }
+                public float GrowthPerTick { get; set; }
+                public bool IsOutdoor { get; set; }
+            };
+            private class Prediction // should contain update rule
+            {
+                public class MinMax
+                {
+                    public bool showDeficiency { get; set; } = false;
+                    private float _min, _max;
+                    public float min
+                    {
+                        get { return _min; }
+                        set
+                        {
+                            _min = value;
+                            if (showDeficiency != true && value < 0)
+                            {
+                                _min = 0;
+                            }
+                        }
+                    }
+                    public float max
+                    {
+                        get { return _max; }
+                        set
+                        {
+                            _max = value;
+                            if (showDeficiency != true && value < 0)
+                            {
+                                _max = 0;
+                            }
+                        }
+                    }
+
+                }
+                public class PredTerm
+                {
+                    public MinMax yield, consumption, stock;
+                    public bool enabled;
+                    public void SetUpdateRule(float v0, float v)
+                    {
+
+                    }
+                    public void UpdateOnce()
+                    {
+
+                    }
+                }
+                private bool _showDeficiency;
+                public bool showDeficiency
+                {
+                    get { return _showDeficiency; }
+                    set
+                    {
+                        //_showDeficiency = hay_yield.showDeficiency = hay_stock.showDeficiency = meat_stock.showDeficiency =
+                         //   animal_population.showDeficiency = hay_consumption.showDeficiency = value;
+                    }
+                }
+                public bool enabled { get; set; }
+
+                public Prediction(MinMax a, MinMax b, MinMax c, MinMax d, MinMax e)
+                {
+                    showDeficiency = false;
+                }
+                public List<PredTerm> PredTerms { get; set; }
+
+                public void update() {
+                    if (enabled)
+                    {
+                        /*
+                        // crop
+                        if (GenDate.HourOfDay >= 4 && GenDate.HourOfDay <= 19)
+                        {
+                            if (allHaygrassGrowth.LastOrDefault().Growth >= 1.0f)
+                            {
+                                allHaygrassGrowth.LastOrDefault().Growth = Plant.BaseGrowthPercent;
+                                projectedRecords[0].hay_yield.max += haygrass_yieldMax;
+                                projectedRecords[0].hay_yield.min += haygrass_yieldMin;
+                            }
+                            projectedRecords[0].hay_stock.max += projectedRecords[0].hay_yield.max;
+                            projectedRecords[0].hay_stock.min += projectedRecords[0].hay_yield.min;
+                        }
+                        stock. -= GenDate.CurrentDayPercent * dailyHayConsumption; // only count the rest of day
+                        projectedRecords[0].hay_stock.min -= GenDate.CurrentDayPercent * dailyHayConsumption;
+                        projectedRecords[0].meat_stock.max -= GenDate.CurrentDayPercent * dailyKibbleConsumption * 2f / 5f; // convert every 50 kibbles to 20 meat
+                        projectedRecords[0].meat_stock.min -= GenDate.CurrentDayPercent * dailyKibbleConsumption * 2f / 5f;
+
+                        // calculate yields and stocks after today
+                        for (int day = 1; day < nextNDays; day++)
+                        {
+                            projectedRecords.Add(
+                                new Prediction(
+                                    new Prediction.MinMax { min = 0, max = 0 },
+                                    new Prediction.MinMax { max = projectedRecords[day - 1].hay_stock.max, min = projectedRecords[day - 1].hay_stock.min },
+                                    new Prediction.MinMax { max = projectedRecords[day - 1].hay_consumption.max, min = projectedRecords[day - 1].hay_consumption.min },
+                                    new Prediction.MinMax { max = projectedRecords[day - 1].meat_stock.max, min = projectedRecords[day - 1].meat_stock.min },
+                                    new Prediction.MinMax { max = projectedRecords[day - 1].animal_population.max, min = projectedRecords[day - 1].animal_population.min }));
+
+                            foreach (PlantGrowth k in allHaygrassGrowth)
+                            {
+                                if (k.IsOutdoor && !(day <= daysUntilGrowingPeriodOver))
+                                {
+                                    break;
+                                }
+                                k.Growth += k.GrowthPerTick * GenDate.TicksPerDay * 0.55f; // 0.55 is 55% of time plant spent growing
+                                                                                           //debug_val[6] = k.GrowthPerTick;
+                                if (k.Growth >= 1.0f)
+                                {
+                                    projectedRecords[day].hay_yield.max += haygrass_yieldMax;
+                                    projectedRecords[day].hay_yield.min += haygrass_yieldMin;
+                                    k.Growth = Plant.BaseGrowthPercent; // if it's fully grown, replant and their growths start at 5%.
+                                }
+                            }
+
+                            projectedRecords[day].hay_stock.max += projectedRecords[day].hay_yield.max;
+                            projectedRecords[day].hay_stock.min += projectedRecords[day].hay_yield.min;
+                            projectedRecords[day].hay_stock.max -= dailyHayConsumption;
+                            projectedRecords[day].hay_stock.min -= dailyHayConsumption;
+                            projectedRecords[day].meat_stock.max -= dailyKibbleConsumption * 2f / 5f;
+                            projectedRecords[day].meat_stock.min -= dailyKibbleConsumption * 2f / 5f;
+                            */
+
+                        }
+                    }
+                
+            };
+            Dictionary<string, Prediction> all;
+            void MakePrediction(int days)
+            {
+                foreach (var t in this.all)
+                {
+                    t.Value.update();
+                }
+            }
+        }
 
         // functions
+        public MainTabWindow_Estimator () : base()
+        {
+            MainTabWindow_Estimator.hayNut = (from d in DefDatabase<ThingDef>.AllDefs.Where(x => x.defName == "Hay")
+                                   select d).FirstOrDefault().ingestible.nutrition;
+            MainTabWindow_Estimator.haygrass_yieldMax = GenMath.RoundRandom(
+                (from d in DefDatabase<ThingDef>.AllDefs.Where(x => x.defName == "PlantHaygrass")
+                 select d).FirstOrDefault().plant.harvestYield);
+            MainTabWindow_Estimator.haygrass_yieldMin = GenMath.RoundRandom(
+                (from d in DefDatabase<ThingDef>.AllDefs.Where(x => x.defName == "PlantHaygrass")
+                 select d).FirstOrDefault().plant.harvestYield * 0.5f * 0.5f// 1st 0.5 is harvesting at 65% growth, 2nd 0.5 is lowest health.
+                );
+        }
         public override Vector2 RequestedTabSize
         {
             get
@@ -325,8 +514,6 @@ namespace FoodAnalyticsTab
                 projectedRecords[day].hay_stock.min -= dailyHayConsumption;
                 projectedRecords[day].meat_stock.max -= dailyKibbleConsumption * 2f / 5f;
                 projectedRecords[day].meat_stock.min -= dailyKibbleConsumption * 2f / 5f;
-
-
             }
         }
 
@@ -421,7 +608,7 @@ namespace FoodAnalyticsTab
             string analysis = "Number of animals you have = " + (int)numAnimals + ", hay animals = " + numRoughAnimals + ", kibble animals = " + numKibbleAnimals +
                             "\nNumber of hay in stockpiles and on the floors = " + (int)numHay + ", number of meat = " + numMeat +
                             "\nEstimated number of hay needed daily for hay-eaters only= " + (int)totalNeededHay +
-                            "\nEstimated number of kibble needed for all kibble-eaters = " + (int)dailyKibbleConsumption +
+                            "\nEstimated number of kibble needed for all kibble-eaters = " + (int)dailyKibbleConsumption + ", meat =" + dailyKibbleConsumption*2/5 + ", egg=" + dailyKibbleConsumption*4/50 +
                             "\nEstimated number of hay needed daily for all animals = " + (int)dailyHayConsumption +
                             "\nNumber of days until hay in stockpiles run out = " + String.Format("{0:0.0}", numHay / dailyHayConsumption) +
                             "\nDays Until Winter = " + this.daysUntilWinter + ", Days until growing period over = " + this.daysUntilGrowingPeriodOver +
@@ -434,6 +621,7 @@ namespace FoodAnalyticsTab
                             "\nEstimated number of hay needed yearly for all animals = " + (int)(dailyHayConsumption * GenDate.DaysPerMonth * GenDate.MonthsPerYear) + // 60 days
                             "\nEstimated number of hay needed until next harvest season(10th of Spring) for all animals = " + (int)(dailyHayConsumption * this.daysUntilNextHarvestSeason) +
                             "\nNumber of haygrass plant = " + (int)numHaygrass + ", outdoor = " + allHaygrassGrowth.Where(h => h.IsOutdoor).Count() +
+                            "\nNumber of haygrass needed = " + dailyHayConsumption / 20 * 10 +
                             "\nEstimate of projected harvest production:\n";
             analysis += "Day\t Max Yield Min Yield Max Stock Min Stock\n";
             int i = 0;
